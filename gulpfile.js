31366089e1009dad20011df65ecb96bff9ee01c2
Ilia Sidash <iliasidash@pik-arenda.ru>
2019-05-19 19:46:40 +0300

    Add task and function html in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,34 +8,48 @@ const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
 const rename = require('gulp-rename');
 
-
-
-
 function styles() {
-    return gulp.src('./src/sass/**/*.sass')
+    return gulp
+        .src('./src/sass/**/*.sass')
         .pipe(sass())
         .pipe(concat('style.css'))
-        .pipe(autoprefixer({
-            browsers: ['last 2 version'],
-            cascade: false
-        }))
-        .pipe(cleanCSS({
-            level: 2
-        }))
-        .pipe(rename({suffix:'.min'}))
+        .pipe(
+            autoprefixer({
+                browsers: ['last 2 version'],
+                cascade: false
+            })
+        )
+        .pipe(
+            cleanCSS({
+                level: 2
+            })
+        )
+        .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('./build/css'))
         .pipe(browserSync.stream());
 }
 
+function html() {
+    return gulp
+        .src('./src/*.html')
+        .pipe(gulp.dest('./build/'))
+        .pipe(browserSync.stream());
+}
+
 function scripts() {
-    return gulp.src('./src/js/**/*.js')
+    return gulp
+        .src('./src/js/**/*.js')
         .pipe(concat('script.js'))
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(uglify({
-            toplevel: true
-        }))
+        .pipe(
+            babel({
+                presets: ['@babel/env']
+            })
+        )
+        .pipe(
+            uglify({
+                toplevel: true
+            })
+        )
         .pipe(gulp.dest('./build/js'))
         .pipe(browserSync.stream());
 }
@@ -44,21 +58,21 @@ function browserReload(done) {
     browserSync.reload();
     done();
 }
-function watchFiles(){
+function watchFiles() {
     gulp.watch('./src/sass/**/*.sass', styles);
     gulp.watch('./src/js/**/*.js', scripts);
-    gulp.watch('./src/**/*.html', browserReload);
+    gulp.watch('./src/**/*.html', html);
 }
 function sync() {
     browserSync.init({
         server: {
-            baseDir: './src'
+            baseDir: 'build'
         },
-            port:3000     
+        port: 3000
     });
 }
 
-gulp.task('default', gulp.series(styles, gulp.parallel(sync, watchFiles)));
-
-
- 
+gulp.task(
+    'default',
+    gulp.series(html, styles, gulp.parallel(sync, watchFiles))
+);
